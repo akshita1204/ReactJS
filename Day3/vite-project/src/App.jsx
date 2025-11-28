0d560982c3333adf108c49f36e188f5e3411175f
Akshita Tyagi <akshitatyagi04@gmail.com>
2025-11-28 20:39:39 +0530

    commit

--- Day3/vite-project/src/App.jsx
+++ Day3/vite-project/src/App.jsx
@@ -1,13 +1,25 @@
-
-
-import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+  let  [count,setCount]= useState(0);
+  function increaseNumber(){
+    count++;
+    setCount(count);
+  }
+
+  // function increaseNumber(){
+  //   count++;
+  //   console.log(count);
+  //   document.querySelector('p').innerText = "Counter: " + count;   //dom manipulation
+  // }
 
   return (
+   
     <>
      <h1>Hello! This is my basic ReactJS Project</h1>
+     <p>Counter: {count}</p>
+     <button onClick={increaseNumber}>Increment</button>
     </>
   )
 }
